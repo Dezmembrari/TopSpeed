4619a58adee688822bc135258e1748933dd99431
Dezmembrari <51934552+Dezmembrari@users.noreply.github.com>
2024-10-17 21:35:15 +0300

    speed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-import '../src/assets/css/styles.scss';
+// import '../src/assets/css/styles.scss';
 import { createApp } from 'vue';
 import { VueReCaptcha } from 'vue-recaptcha-v3';
 import App from './App.vue';
